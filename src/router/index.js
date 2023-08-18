12a4804566578bc45d3a8e115452ad67e165732a
WhiteNight <1448375249@qq.com>
2023-08-18 20:41:29 +0800

    完善一些功能

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,15 @@ import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import My from "../views/My.vue";
+import Welcome from "../views/Welcome.vue";
+import Nopage from "../views/404.vue";
+import Interface from "../views/Interface.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Login",
-    component: Login,
+    name: "Welcom",
+    component: Welcome,
   },
   {
     path: "/home",
@@ -29,6 +32,16 @@ const routes = [
     path: "/my",
     name: "My",
     component: My,
+  },
+  {
+    path: "/interface",
+    name: "Interface",
+    component: Interface,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "404",
+    component: Nopage,
   },
 ];
 
---
+++ src/views/404.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="page-404">
+        <h1>404</h1>
+        <p>页面不存在</p>
+        <router-link to="/">返回首页</router-link>
+    </div>
+</template>
