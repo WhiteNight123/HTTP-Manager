4a245b0351d42cc653410f1cc456fa8f23f21ce7
WhiteNight <1448375249@qq.com>
2023-12-12 11:27:08 +0800

    完善项目配色

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "element-plus/dist/index.css";
-import 'element-plus/theme-chalk/dark/css-vars.css'
+import "./styles/index.scss";
+import "element-plus/theme-chalk/dark/css-vars.css";
 
 const app = createApp(App);
 app.use(router);
